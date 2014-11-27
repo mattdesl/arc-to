9e70074d42eb6b703fc0b17f3f5ba6c045a00a5c
mattdesl <dave.des@gmail.com>
2014-11-27 12:20:25 -0500

    removing noise

--- index.js
+++ index.js
@@ -8,38 +8,38 @@ module.exports = function arc(x, y, radius, start, end, clockwise, steps, path) 
     radius = radius||0
     start = start||0
     end = end||0
-    
+
     //determine distance between the two angles
     //...probably a nicer way of writing this
-    var dist = Math.abs(start-end);
+    var dist = Math.abs(start-end)
     if (!clockwise && start > end)
-        dist = 2*Math.PI - dist;
+        dist = 2*Math.PI - dist
     else if (clockwise && end > start)
-        dist = 2*Math.PI - dist;
+        dist = 2*Math.PI - dist
 
     //approximate the # of steps using the cube root of the radius
     if (typeof steps !== 'number') 
-        steps = Math.max(6, Math.floor(6 * Math.pow(radius, 1/3) * (dist / (Math.PI))));
+        steps = Math.max(6, Math.floor(6 * Math.pow(radius, 1/3) * (dist / (Math.PI))))
 
     //ensure we have at least 3 steps..
-    steps = Math.max(steps, 3);
+    steps = Math.max(steps, 3)
 
     var f = dist / (steps),
-        t = start;
-        
+        t = start
+
     //modify direction
-    f *= clockwise ? -1 : 1;
+    f *= clockwise ? -1 : 1
 
     for (var i=0; i<steps+1; i++) {
         var cs = Math.cos(t),
-            sn = Math.sin(t);
+            sn = Math.sin(t)
 
         var nx = x + cs*radius,
-            ny = y + sn*radius;
+            ny = y + sn*radius
 
-        path.push([ nx, ny ]);
+        path.push([ nx, ny ])
 
-        t += f;
+        t += f
     }
-    return path;
-};
+    return path
+}
